fe7bc3158964eebd1c667d4a076e2cdceb697235
ice breaker <1324318532@qq.com>
2025-02-24 23:54:55 +0800

    chore: commit codes

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,7 @@
 import { defineConfig, type UserConfigExport } from '@tarojs/cli'
+import type { Plugin } from 'vite'
+import tailwindcss from 'tailwindcss'
+import { UnifiedViteWeappTailwindcssPlugin as uvtw } from 'weapp-tailwindcss/vite'
 
 import devConfig from './dev'
 import prodConfig from './prod'
@@ -27,7 +30,29 @@ export default defineConfig<'vite'>(async (merge, { command, mode }) => {
       }
     },
     framework: 'react',
-    compiler: 'vite',
+    compiler: {
+      type: 'vite',
+      vitePlugins: [
+        {
+          // 通过 vite 插件加载 postcss,
+          name: 'postcss-config-loader-plugin',
+          config(config) {
+            // 加载 tailwindcss
+            if (typeof config.css?.postcss === 'object') {
+              config.css?.postcss.plugins?.unshift(tailwindcss())
+            }
+          },
+        },
+        uvtw({
+          // rem转rpx
+          rem2rpx: true,
+          // 除了小程序这些，其他平台都 disable
+          disabled: process.env.TARO_ENV === 'h5' || process.env.TARO_ENV === 'harmony' || process.env.TARO_ENV === 'rn',
+          // 由于 taro vite 默认会移除所有的 tailwindcss css 变量，所以一定要开启这个配置，进行css 变量的重新注入
+          injectAdditionalCssVarScope: true,
+        })
+      ] as Plugin[] // 从 vite 引入 type, 为了智能提示
+    },
     mini: {
       postcss: {
         pxtransform: {
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,22 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
-import './index.css'
+import './index.scss'
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
   })
 
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
+    <View>
+      <View className='bg-[#89ab8d] flex flex-col'>
+        <View className='text-[#438821] dark:text-[#ec4f4f] before:content-["11111"]'>Hello world!</View>
+        <View className='text-[#3d31a4] before:content-["222"]'>Hello world!</View>
+      </View>
+      <View className='bg-[#89ab8d] flex divide-x-8 divide-solid divide-[#60d256]'>
+        <View className='text-[#438821] dark:text-[#ec4f4f] before:content-["11111"]'>Hello world!</View>
+        <View className='text-[#3d31a4] before:content-["222"]'>Hello world!</View>
+      </View>
     </View>
   )
 }
