4bf211b9dbfc2f8c80d59c3ef3817b72eaa1aac4
ice breaker <1324318532@qq.com>
2025-03-30 21:03:57 +0800

    chore(deps): upgrade

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,14 +2,18 @@ import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.css'
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
   })
 
   return (
-    <View className='index bg-[#123456]'>
-      <Text className='text-[55rpx] text-[#fff]'>Hello world!</Text>
+    <View>
+      <Text className='text-[#123456] bg-amber-100'>Weapp-tailwindcss 模板</Text>
+      <View className='index bg-[#123456]'>
+        <Text className='text-[55rpx] text-[#fff]'>Hello world!</Text>
+      </View>
     </View>
+
   )
 }
