2af2f13b4ad485306b776603290e048426af5e98
ice breaker <1324318532@qq.com>
2025-06-25 19:25:10 +0800

    chore: 锁定 vite@4 版本

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <View className='index bg-[#123456]'>
         <Text className='text-[55rpx] text-[#aae90b]'>Hello world!</Text>
       </View>
-      <View className="h-14 bg-linear-to-r from-cyan-500 to-blue-500"></View>
-      <View className="h-14 bg-linear-to-t from-sky-500 to-indigo-500"></View>
-      <View className="h-14 bg-linear-to-bl from-violet-500 to-fuchsia-500"></View>
-      <View className="h-14 bg-linear-65 from-purple-500 to-pink-500"></View>
+      <View className='h-14 bg-linear-to-r from-cyan-500 to-blue-500'></View>
+      <View className='h-14 bg-linear-to-t from-sky-500 to-indigo-500'></View>
+      <View className='h-14 bg-linear-to-bl from-violet-500 to-fuchsia-500'></View>
+      <View className='h-14 bg-linear-65 from-purple-500 to-pink-500'></View>
     </View>
 
   )
